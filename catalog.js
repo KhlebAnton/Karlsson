ab428ec9804d2879d4ada4a097e245dc6131de30
Anton Khlebnikov <toni.farsh@icloud.com>
2024-05-07 12:03:58 +0500

    fix

--- catalog.js
+++ catalog.js
@@ -108,33 +108,3 @@ function replaceShop(data) {
         }
     });
 }
-
-const hamb = document.querySelector("#hamb");
-const popup = document.querySelector("#popup");
-const body = document.body;
-
-
-
-// При клике на иконку hamb вызываем ф-ию hambHandler
-hamb.addEventListener("click", hambHandler);
-
-// Выполняем действия при клике ..
-function hambHandler(e) {
-    e.preventDefault();
-    // Переключаем стили элементов при клике
-    popup.classList.toggle("open");
-    hamb.classList.toggle("active");
-    body.classList.toggle("noscroll");
-}
-
-
-
-
-// Закрытие попапа при клике на меню
-function closeOnClick() {
-    popup.classList.remove("open");
-    hamb.classList.remove("active");
-    body.classList.remove("noscroll");
-}
-
-
